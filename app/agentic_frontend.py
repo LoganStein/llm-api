a2dbedb89165f00b5113ed30ac12e5ff3b7a4746
Logan Stein <ljstein12@gmail.com>
2024-11-08 14:02:19 -0600

    connect streamlit ui to autogen agents

---
+++ app/agentic_frontend.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import time
+from agentic_model import run_model
+
+
+def main():
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    if "ai_typing" not in st.session_state:
+        # could be good to have a typing indicator for the AI
+        st.session_state.ai_typing = False
+
+    # header
+    st.title("Welcome to My Streamlit App")
+    st.write("This is a simple Streamlit boilerplate.")
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = run_model(prompt)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response})
+
+
+if __name__ == "__main__":
+    main()
